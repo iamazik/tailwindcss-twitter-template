706964f1d7d1cd75cb104e4e9ecbf4c32648339f
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2019-08-08 23:46:08 +0600

    Add Purgecss support

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./public/**/*.html'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+})
+
 module.exports = {
-  plugins: {
-    autoprefixer: {},
-    tailwindcss: {},
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss,]
+      : [],
+  ]
 }
