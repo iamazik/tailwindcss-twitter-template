8fa697995e8f24730eb6aad1a7969c8b14bd5a50
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2019-08-08 23:52:13 +0600

    Add cssnano support

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,7 @@ module.exports = {
     require('tailwindcss'),
     require('autoprefixer'),
     ...process.env.NODE_ENV === 'production'
-      ? [purgecss,]
+      ? [purgecss, require('cssnano'),]
       : [],
   ]
 }
